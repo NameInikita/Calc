21a73c850dfc74ae070de9956904ef2c848560b8
Nikita <nikitagorinvg@gmail.com>
2017-04-22 23:12:27 +0300

    ready

---
+++ Calc/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        delegate double MyDelegate(int a, int b);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое числ: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе числ: ");
+            int j = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите операцию: + , - , / , * ");
+            string str = Convert.ToString(Console.ReadLine());
+
+            MyDelegate myDel = null;
+
+            switch (str)
+            {
+                case "+":
+                    myDel = (a, b) => { return a + b; };
+                    break;
+                case "-":
+                    myDel = (x, y) => { return x - y; };
+                    break;
+                case "*":
+                    myDel = (f, r) => { return f * r; };
+                    break;
+                case "/":
+                    myDel = (s, k) =>
+                    {
+                        if (k != 0)
+                        {
+                            return s / (double)k;
+                        }
+                        else
+                        {
+                            Console.WriteLine("На ноль делить незя");
+                            return 0;
+                        }
+                    };//завершаем лямбда оператор
+                    break;
+                default:
+                    Console.WriteLine("Введен несуществующии оператор");
+                    break;
+            }
+
+            //!!!!!!!!!еслиделегат не равен null то выполняется соответствующий метод, кот сообщен с делегатом
+            if (myDel != null)
+                Console.WriteLine("Значенние операции {0} равно {1:##.###}", str, myDel(i, j));
+
+            Console.ReadLine();
+        }
+    }
+}
